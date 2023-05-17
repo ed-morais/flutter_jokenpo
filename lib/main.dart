e18dcf09a73d16653c06791e17ad976b4cef1a0d
Tiago Silva <87677785+TSCTiago@users.noreply.github.com>
2023-05-17 19:11:49 -0300

    initial structure

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
       title: 'JOKENPO',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('JOKENPÔ'),
+          // title: const Text('JOKENPÔ'),
           centerTitle: true,
         ),
-        body: const Column(
+        body: Column(
           children: [
-            Text('TESTANDO'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    const Text('01'),
+                    Image.network(
+                      'https://art.pixilart.com/1a6c64727dac4a7.png',
+                      width: 100,
+                    ),
+                    const Text('DERROTAS'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text('04'),
+                    Image.network(
+                      'https://art.pixilart.com/1a6c64727dac4a7.png',
+                      width: 100,
+                    ),
+                    const Text('DERROTAS'),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const Text('03'),
+                    Image.network(
+                      'https://art.pixilart.com/1a6c64727dac4a7.png',
+                      width: 100,
+                    ),
+                    const Text('DERROTAS'),
+                  ],
+                ),
+              ],
+            ),
+            const Text('VOCÊ GANHOU'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.network(
+                  'https://img.lovepik.com/free-png/20211216/lovepik-scissors-png-image_401704654_wh1200.png',
+                  width: 190,
+                ),
+                Image.network(
+                  'https://img.lovepik.com/free-png/20211216/lovepik-scissors-png-image_401704654_wh1200.png',
+                  width: 190,
+                ),
+              ],
+            )
           ],
         ),
       ),
